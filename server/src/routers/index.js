a759ff9612f87d7dc9269c5982d95f566b32ffce
박재인 <mangowodls@gmail.com>
2023-01-09 18:11:40 +0900

    feat: /router/index.js 수정

---
+++ server/src/routers/index.js
@@ -0,0 +1,5 @@
+const userRouter = require("./user_router");
+const boardRouter = require("./board_router");
+const authRouter = require("./auth_router");
+
+module.exports = { userRouter, boardRouter, authRouter }

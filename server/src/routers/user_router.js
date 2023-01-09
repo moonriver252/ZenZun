034a3f7223fdb9ce6dd13c9004d9f510db83c28a
박재인 <mangowodls@gmail.com>
2023-01-09 18:28:42 +0900

    feat: user_router 회원가입 기능 작성

---
+++ server/src/routers/user_router.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const userRouter = express.Router();
+//const upload = require("../middlewares/upload");
+
+// const { loginRequired } = require("../middlewares/login_required");
+// const {
+//   userService,
+//   userTagService,
+//   userRefreshTokenService,
+// } = require("../service");
+
+
+//회원가입
+userRouter.post("/", async (req, res, next) => {
+    try {
+        const email = req.body.email;
+        const nickname = req.body.nickname;
+        const profile_image = req.body.profile_image;
+        const password = req.body.password;
+
+        const newUser = await userService.addUser({
+            email,
+            nickname,
+            profile_image,
+            password
+        });
+
+        res.status(201).json(newUser);
+        } catch (error) {
+            next(error)
+        }
+})

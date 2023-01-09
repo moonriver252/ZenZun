42bf1dad40c13f28a1aff6db126568852c8cfe7f
moonriver252 <moonriver252@gmail.com>
2023-01-09 18:13:36 +0900

    feat: service/index.js 수정

---
+++ server/src/service/index.js
@@ -0,0 +1,5 @@
+const userService = require("./user_service");
+const boardService = require("./board_service");
+const authService = require("./auth_service");
+
+module.exports = { userService, boardService, authService }

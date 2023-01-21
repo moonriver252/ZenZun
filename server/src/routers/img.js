c46d653d89ebb243e73187354acbbbb9bbdc4409
moonriver252 <moonriver252@gmail.com>
2023-01-21 20:07:46 +0900

    fix: 불필요한 파일 삭제

--- server/src/routers/img.js
+++
@@ -1,9 +0,0 @@
-// const awsS3 = require("../middlewares/aws-s3");
-// const express = require("express");
-// const img = express.Router();
-
-// img.post('/img', awsS3.single('image'), (req,res)=>{
-//     res.send('good!')
-// })
-
-// module.exports = img;
